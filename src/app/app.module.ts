ed2a525919a24830ecb20c68bedc68a3fc9092fd
Dh4ki <edu_gv_14@hotmail.com>
2023-05-23 17:23:42 -0500

    asd

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { routing } from "./app.routing";
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { PerfilComponent } from './components/usuario/perfil/perfil.component';
+import { SidebarComponent } from './components/usuario/sidebar/sidebar.component';
+import { IndexProductoComponent } from './components/productos/index-producto/index-producto.component';
+import { ShowProductoComponent } from './components/productos/show-producto/show-producto.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    NavComponent,
+    FooterComponent,
+    LoginComponent,
+    PerfilComponent,
+    SidebarComponent,
+    IndexProductoComponent,
+    ShowProductoComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    routing,
+    NgbPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
